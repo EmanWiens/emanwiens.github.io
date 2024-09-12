5220a8ab728708e88d55a35a576cb5014565ab25
emanwiens <emanwiens@gmail.com>
2024-09-12 16:26:04 -0500

    Updates

---
+++ precache-manifest.333cffc0f6163eb083c1c8d531db2977.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac29b0d6f3d18ca338fdd2c0d6ff44b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c82dc6a22909ceb79220",
+    "url": "/react-gh-pages/static/css/main.128a6c1c.chunk.css"
+  },
+  {
+    "revision": "de4f85bdefef81cda658",
+    "url": "/react-gh-pages/static/js/2.74262d08.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-gh-pages/static/js/2.74262d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599e9838dc38c153d997",
+    "url": "/react-gh-pages/static/js/3.e0826ac2.chunk.js"
+  },
+  {
+    "revision": "c82dc6a22909ceb79220",
+    "url": "/react-gh-pages/static/js/main.472e9294.chunk.js"
+  },
+  {
+    "revision": "b2dc830cfbf595adb1fc",
+    "url": "/react-gh-pages/static/js/runtime-main.b035f971.js"
+  },
+  {
+    "revision": "ef5fd4c6374abd3744b6746e6bd13971",
+    "url": "/react-gh-pages/static/media/RERT.ef5fd4c6.gif"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  }
+]);
