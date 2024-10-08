01b160063645672e4879b8702433b50c593c68cb
Emanuel Wiens <EmanWiens@gmail.com>
2024-10-08 18:19:26 -0500

    nav bar for mobile works

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -4,19 +4,26 @@ const NavigationBar = () => {
     return (
       <>
         <nav>
-      <div className="logo">
-       <NavLink to=""><h2>Emanuel Wiens</h2></NavLink>
-      </div>
-      <div className="nav-items">
-        <ul className="overview">
-          <li><NavLink className="nav_bar" to="">Home</NavLink></li>
-          <li><NavLink className="nav_bar" to="projects">Projects</NavLink></li>
-          <li><NavLink className="nav_bar" to="plants">Plants</NavLink></li>
-          <li><NavLink className="nav_bar" to="guides">Guides</NavLink></li>
-          <li><NavLink className="nav_bar" to="about">About</NavLink></li>
-        </ul>
-      </div>
-    </nav>
+          <div className="logo">
+            <NavLink to=""><h2>Emanuel Wiens</h2></NavLink>
+          </div>
+
+          <div className="nav-items_mobile">
+            <ul className="overview_mobile">
+              <li><NavLink className="nav_bar" to="">Menu</NavLink></li>
+            </ul>
+
+            <div className="nav-items">
+              <ul className="overview">
+                <li><NavLink className="nav_bar" to="">Home</NavLink></li>
+                <li><NavLink className="nav_bar" to="projects">Projects</NavLink></li>
+                <li><NavLink className="nav_bar" to="plants">Plants</NavLink></li>
+                <li><NavLink className="nav_bar" to="guides">Guides</NavLink></li>
+                <li><NavLink className="nav_bar" to="about">About</NavLink></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </>
     );
   }
