ce6be5b5f8a024a0c15ca749aeaf42406072c3bb
Emanuel Wiens <EmanWiens@gmail.com>
2024-10-15 21:51:25 -0500

    Content is now rendered instead of generated

--- src/Utils.js
+++ src/Utils.js
@@ -106,6 +106,16 @@ const render_iteration = (iteration_dict, index) => {
     blurb_elements.push(render_blurb(iteration_dict[index]["blurbs"], blurb)); 
   }
 
+  var render_components = null; 
+  if (iteration_dict[index]["components"].length >= 1) {
+    <div className="components_div">
+      <h4>Components</h4>
+      <ul>
+        {component_elements}
+      </ul>
+    </div>
+  }
+
   return (
     <div className="iteration_div">
       <div className="iteration_header_div">
@@ -113,12 +123,7 @@ const render_iteration = (iteration_dict, index) => {
         <p>{iteration_dict[index]["introduction"]}</p>
       </div>
 
-      <div className="components_div">
-        <h4>Components</h4>
-        <ul>
-          {component_elements}
-        </ul>
-      </div>
+      {render_components}
 
       <div className="blurbs_div">
         {blurb_elements}
@@ -139,7 +144,6 @@ const render_blurb = (blurb_dict, index) => {
   var right_blurb = null; 
   var left_blurb = null; 
   var whole_blurb = null; 
-  var code = false; 
 
   if (blurb_dict[index]["images"].length >= 1) {
     right_blurb = (
@@ -188,157 +192,58 @@ const render_blurb = (blurb_dict, index) => {
   ); 
 }
 
-
-function create_element(dict, index) {
-  var element_html = ""; 
-  var path; 
-  var left = true; 
-  var code = false; 
-
-  if (dict[index]["publish"] === true) {
-    // Add the title and introduction
-    element_html = format("<div className=\"project_div\">");
-    element_html = format("{0}<h2>{1}</h2>", element_html, dict[index]['name']);
-    element_html = format("{0}<p>{1}</p>", element_html, dict[index]["introduction"]);
-
-    // add each iteration of the project 
-    for (let iteration = 0; iteration < dict[index]["iterations"].length; iteration++) {
-      if (dict[index]["iterations"][iteration]["publish"] === true) {
-        element_html = format("{0}<div className=\"iteration_div\">", element_html);
-        element_html = format("{0}<h3>{1}</h3>", element_html, dict[index]["iterations"][iteration]["title"]);
-        element_html = format("{0}<p>{1}</p>", element_html, dict[index]["iterations"][iteration]["introduction"]);
-
-        // add components list 
-        if (dict[index]["iterations"][iteration]["components"].length >= 1) {
-          element_html = format("{0}<h4>{1}</h4>", element_html, "Components");
-          element_html = format("{0}<div className=\"blurb_div\">", element_html);
-          element_html = format("{0}<ul>", element_html, "Components");
-
-          for (let component = 0; component < dict[index]["iterations"][iteration]["components"].length; component++) {
-
-            // if components changed over iterations use "old_link" and "old_text"
-            if (dict[index]["iterations"][iteration]["components"][component]["old_text"] === "") {
-              element_html = format("{0}<li><a href=\"{1}\">{2}</a></li>", element_html, dict[index]["iterations"][iteration]["components"][component]["link"], dict[index]["iterations"][iteration]["components"][component]["text"]);
-            } else {
-              element_html = format("{0}<li><strike><a href=\"{1}\">{2}</a></strike> → <a href=\"{3}\">{4}</a></li>", element_html, dict[index]["iterations"][iteration]["components"][component]["old_link"], dict[index]["iterations"][iteration]["components"][component]["old_text"], dict[index]["iterations"][iteration]["components"][component]["link"], dict[index]["iterations"][iteration]["components"][component]["text"]);
-            }
-
-          }
-          element_html = format("{0}</ul></div>", element_html);  // close iteration_div
-        }
-
-        // Add all the blurbs for the project 
-        for (let blurb = 0; blurb < dict[index]["iterations"][iteration]["blurbs"].length; blurb++) {
-          element_html = format("{0}<br/><h4>{1}</h4>", element_html, dict[index]["iterations"][iteration]["blurbs"][blurb]["title"]);
-          element_html = format("{0}<div className=\"blurb_div\">", element_html);
-          code = "code" in dict[index]["iterations"][iteration]["blurbs"][blurb];
-
-          // if it is just a blurb, don't add another div
-          // if there is an image, alternate adding in right_blurb_item
-          if (dict[index]["iterations"][iteration]["blurbs"][blurb]["images"].length >= 1 || (code && dict[index]["iterations"][iteration]["blurbs"][blurb]["code"])) {
-            if (left) {
-              element_html = format("{0}<div className=\"left_blurb_item\">", element_html);
-            } else {
-              element_html = format("{0}<div className=\"right_blurb_item\">", element_html);
-            }
-            left = !left;
-          }
-          
-          element_html = format("{0}<p>{1}</p>", element_html, dict[index]["iterations"][iteration]["blurbs"][blurb]["text"]);
-          if (dict[index]["iterations"][iteration]["blurbs"][blurb]["images"].length >= 1 || (code && dict[index]["iterations"][iteration]["blurbs"][blurb]["code"])) {
-            element_html = format("{0}</div>", element_html); // close blurb_item
-          }
-
-          // place the images 
-          if (dict[index]["iterations"][iteration]["blurbs"][blurb]["images"].length >= 1 || (code && dict[index]["iterations"][iteration]["blurbs"][blurb]["code"])) {
-            if (left) {
-              element_html = format("{0}<div className=\"left_blurb_item{1}\">", element_html, code ? " code" : "");
-            } else {
-              element_html = format("{0}<div className=\"right_blurb_item{1}\">", element_html, code ? " code" : "");
-            }
-          }
-
-          if (code) {
-            element_html = format("{0}{1}", element_html, dict[index]["iterations"][iteration]["blurbs"][blurb]["code"]);
-          } else {
-            for (let image = 0; image < dict[index]["iterations"][iteration]["blurbs"][blurb]["images"].length; image++) {
-              path = map_images(dict[index]["iterations"][iteration]["blurbs"][blurb]["images"][image]["name"]); 
-
-              if (path != null) {
-                element_html = format("{0}<img src=\"{1}\" alt=\"{2}\" />", element_html, path, dict[index]["iterations"][iteration]["blurbs"][blurb]["images"][image]["alt"]);
-              }
-            }
-          }
-
-          if (dict[index]["iterations"][iteration]["blurbs"][blurb]["images"].length >= 1 || (code && dict[index]["iterations"][iteration]["blurbs"][blurb]["code"])) {
-            element_html = format("{0}</div>", element_html); // close blurb_item
-          }
-
-          element_html = format("{0}</div>", element_html); // close blurb_div
-        }
-
-        element_html = format("{0}</div>", element_html); // close iteration_div
-      }
-    }
-
-    element_html = format("{0}</div>", element_html); // close project_div
-  }
-  
-  return element_html; 
-}
-
 export const create_electronics_page = () => {
-  var electronics_html = ""; 
+  var electronic_elements = []; 
 
   for (let electronics = 0; electronics < Data['electronics'].length; electronics++) {
-    electronics_html = format("{0}{1}", electronics_html, create_element(Data['electronics'], electronics)); 
+    electronic_elements.push(render_element(Data["electronics"], electronics)); 
   }
 
   return (
     <>
-      {parse(electronics_html)}
+      {electronic_elements}
     </>
   );
 }
 
 export const create_guides_page = () => {
-  var guides_html = ""; 
+  var guide_elements = []; 
 
-  for (let guide = 0; guide < Data['guides'].length; guide++) {
-    guides_html = format("{0}{1}", guides_html, create_element(Data['guides'], guide)); 
+  for (let guides = 0; guides < Data['guides'].length; guides++) {
+    guide_elements.push(render_element(Data["guides"], guides)); 
   }
 
   return (
     <>
-      {parse(guides_html)}
+      {guide_elements}
     </>
   );
 }
 
 export const create_plants_page = () => {
-  var plants_html = ""; 
+  var plant_elements = []; 
 
-  for (let plant = 0; plant < Data['plants'].length; plant++) {
-    plants_html = format("{0}{1}", plants_html, create_element(Data['plants'], plant)); 
+  for (let plants = 0; plants < Data['plants'].length; plants++) {
+    plant_elements.push(render_element(Data["plants"], plants)); 
   }
 
   return (
     <>
-      {parse(plants_html)}
+      {plant_elements}
     </>
   );
 }
 
 export const create_programming_page = () => {
-  var programming_html = ""; 
+  var programming_elements = []; 
 
-  for (let plant = 0; plant < Data['programming'].length; plant++) {
-    programming_html = format("{0}{1}", programming_html, create_element(Data['programming'], plant)); 
+  for (let programming_counter = 0; programming_counter < Data['programming'].length; programming_counter++) {
+    programming_elements.push(render_element(Data["programming"], programming_counter)); 
   }
 
   return (
     <>
-      {parse(programming_html)}
+      {programming_elements}
     </>
   );
 }
@@ -352,7 +257,7 @@ export const test_render = () => {
 
   return (
     <>
-      {elements}
+      {/* {elements} */}
     </>
   );
 }
